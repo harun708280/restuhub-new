296ac3fa5373fc6cf7c067c7001ba27d7fb9a3f7
Harun <harun708280@gmail.com>
2025-12-22 21:26:05 +0600

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,25 +1,28 @@
 export const dynamic = "force-dynamic";
+
 import { MetadataRoute } from "next";
 
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
- 
+  const baseUrl = "https://restruhub.com";
 
- 
   return [
     {
-      url: `https://restruhub.com/`,
+      url: `${baseUrl}/`,
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
-      url: `https://restruhub.com/privacy`,
+      url: `${baseUrl}/privacy`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
     },
     {
-      url: `https://restruhub.com/Terms&Conditions`,
+      url: `${baseUrl}/terms-and-conditions`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
     },
-    
-    
   ];
 }
